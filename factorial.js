8bd6bd2190a6836088956880bf554f2f8215bc9e
fereshteh <fe.aboohamzeh@gmail.com>
2021-02-06 21:57:13 +0330

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,3 +1,17 @@
+//for loop solution
+function factorial(n){
+ let fact = 1;
+ for(let i=1; i<= n; i++){
+   fact = fact * i     
+ }
+
+  return fact
+}
+// example:
+// console.log(factorial(5)) */
+// => 120
+
+
 function fact(n){
     // when comes into the recursion,
     // the input parameter will read from local scope
